e00d06f478e6630dfc4833cf3e6b785210bb1b4f
CleverNucleus <jamie@mclean007.plus.com>
2021-06-22 16:49:55 +0100

    Reset for fabric

--- src/main/java/com/github/clevernucleus/relicex/RelicEx.java
+++ src/main/java/com/github/clevernucleus/relicex/RelicEx.java
@@ -1,19 +1,12 @@
 package com.github.clevernucleus.relicex;
 
-import com.github.clevernucleus.relicex.init.Registry;
-import com.github.clevernucleus.relicex.util.CommonConfig;
+import net.fabricmc.api.ModInitializer;
 
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-@Mod(RelicEx.MODID)
-public class RelicEx {
+public final class RelicEx implements ModInitializer {
 	public static final String MODID = "relicex";
 	
-	public RelicEx() {
-		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.COMMON_SPEC);
-		Registry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+	@Override
+	public void onInitialize() {
+		
 	}
 }
--- src/main/java/com/github/clevernucleus/relicex/init/item/ILoot.java
+++
@@ -1,5 +0,0 @@
-package com.github.clevernucleus.relicex.init.item;
-
-public interface ILoot {
-	float weight();
-}
