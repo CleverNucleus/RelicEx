01ee045c3d16d3384d882edcc6c1adaeffba5c94
CleverNucleus <jamie@mclean007.plus.com>
2021-09-10 23:14:14 +0100

    Built and ready to update

--- src/main/java/com/github/clevernucleus/relicex/client/RelicExClient.java
+++ src/main/java/com/github/clevernucleus/relicex/client/RelicExClient.java
@@ -2,7 +2,7 @@ package com.github.clevernucleus.relicex.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public final class RelicEx implements ClientModInitializer {
+public final class RelicExClient implements ClientModInitializer {
 	
 	@Override
 	public void onInitializeClient() {
