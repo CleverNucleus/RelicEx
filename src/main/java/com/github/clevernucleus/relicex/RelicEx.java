e455c77feee0eeefd3d194fed20d46539577a463
CleverNucleus <jamie@mclean007.plus.com>
2022-08-06 21:06:36 +0100

    Added drops to chests and ender dragon
    *Mod is ready for release, bar awaiting for retextured relics and relic armor models.

--- src/main/java/com/github/clevernucleus/relicex/RelicEx.java
+++ src/main/java/com/github/clevernucleus/relicex/RelicEx.java
@@ -18,7 +18,16 @@ import com.github.clevernucleus.relicex.item.TomeItem;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v2.LootTableSource;
 import net.minecraft.item.Item;
+import net.minecraft.loot.LootManager;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -59,12 +68,35 @@ public class RelicEx implements ModInitializer {
 		return Registry.register(Registry.ITEM, new Identifier(MODID, keyIn), itemIn);
 	}
 	
+	private static void addLoot(ResourceManager resourceManager, LootManager lootManager, Identifier id, LootTable.Builder tableBuilder, LootTableSource source) {
+		if(config().dragonDropsStone && id.toString().equals("minecraft:entities/ender_dragon")) {
+			LootPool.Builder stone = LootPool.builder().with(ItemEntry.builder(DRAGON_STONE));
+			tableBuilder.pool(stone.build());
+		}
+		
+		if(!config().chestsHaveLoot || !id.toString().contains(":chests/")) return;
+		LootPool.Builder relics = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.01F * (float)config().chestsHaveRelicChance));
+		RELICS.forEach(item -> relics.with(ItemEntry.builder(item)));
+		
+		LootPool.Builder lesserOrb = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.01F * (float)config().chestsHaveLesserOrbChance));
+		lesserOrb.with(ItemEntry.builder(LESSER_ORB_OF_REGRET));
+		
+		LootPool.Builder greaterOrb = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.01F * (float)config().chestsHaveGreaterOrbChance));
+		greaterOrb.with(ItemEntry.builder(GREATER_ORB_OF_REGRET));
+		
+		LootPool.Builder tome = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.01F * (float)config().chestsHaveTomeChance));
+		tome.with(ItemEntry.builder(TOME));
+		
+		tableBuilder.pool(relics.build()).pool(lesserOrb.build()).pool(greaterOrb.build()).pool(tome.build());
+	}
+	
 	@Override
 	public void onInitialize() {
 		AutoConfig.register(RelicExConfig.class, GsonConfigSerializer::new);
 		Registry.register(Registry.SOUND_EVENT, LEVEL_REFUND_SOUND.getId(), LEVEL_REFUND_SOUND);
 		Registry.register(Registry.SOUND_EVENT, POTION_USE_SOUND.getId(), POTION_USE_SOUND);
 		AttributesReloadedEvent.EVENT.register(RARITY_MANAGER::onPropertiesLoaded);
+		LootTableEvents.MODIFY.register(RelicEx::addLoot);
 	}
 	
 	public static RelicExConfig config() {
